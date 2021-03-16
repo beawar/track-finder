8c03855d6e96ec6adb614ce89d1568d090435787
efrank93 <e.franchetti93@gmail.com>
2021-03-16 23:29:17 +0100

    TRAK-15 Fix merge develop

--- src/main/java/com/dovendev/track/jpa/repositories/TrackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dovendev.track.jpa.repositories;
-
-import com.dovendev.track.jpa.entities.Track;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrackRepository extends JpaRepository<Track, Long> {
-}
--- src/main/java/com/dovendev/track/jpa/repositories/TrackResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dovendev.track.jpa.repositories;
-
-import com.dovendev.track.jpa.entities.TracksResources;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrackResourceRepository extends JpaRepository<TracksResources, Long> {
-}
