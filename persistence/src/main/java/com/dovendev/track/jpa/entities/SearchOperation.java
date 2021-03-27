c667995666982006222ca736740bff6398a82ee7
Beatrice Guerra <betrix.pongo@gmail.com>
2021-03-27 03:19:38 +0100

    TRAk-22 remove commented code

--- persistence/src/main/java/com/dovendev/track/jpa/entities/SearchOperation.java
+++ persistence/src/main/java/com/dovendev/track/jpa/entities/SearchOperation.java
@@ -5,19 +5,6 @@ import org.springframework.data.domain.Sort;
 public enum SearchOperation {
   EQUALITY, NEGATION, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN, LESS_THAN_EQUAL, LIKE, STARTS_WITH, ENDS_WITH, CONTAINS;
 
-//  public static final String[] SIMPLE_OPERATION_SET = { ":", "!", ">", "<", "~" };
-//
-//  public static SearchOperation getSimpleOperation(char input) {
-//    return switch (input) {
-//      case ':' -> EQUALITY;
-//      case '!' -> NEGATION;
-//      case '>' -> GREATER_THAN;
-//      case '<' -> LESS_THAN;
-//      case '~' -> LIKE;
-//      default -> null;
-//    };
-//  }
-
   public static SearchOperation getOperationFromSort(Sort.Direction sort) {
     return switch (sort) {
       case ASC -> GREATER_THAN_EQUAL;
--- persistence/src/main/java/com/dovendev/track/jpa/services/TrackService.java
+++ persistence/src/main/java/com/dovendev/track/jpa/services/TrackService.java
@@ -4,21 +4,17 @@ import com.dovendev.track.jpa.entities.QTrack;
 import com.dovendev.track.jpa.entities.SearchOperation;
 import com.dovendev.track.jpa.entities.Track;
 import com.dovendev.track.jpa.entities.TrackSort;
-import com.dovendev.track.jpa.predicates.SearchCriteria;
 import com.dovendev.track.jpa.predicates.TrackPredicatesBuilder;
 import com.dovendev.track.jpa.repositories.TrackRepository;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -49,14 +45,6 @@ public class TrackService {
   public List<Track> findByTitleDescription(String searchText) {
     return trackRepository.findByTitleDescription(searchText);
   }
-
-  //  public List<Track> findAll(int limit, Long cursor){
-  //    Pageable pageable = PageRequest.of(0, limit, Sort.by(Direction.DESC, "uploadTime"));
-  //    if(cursor == null){
-  //      return trackRepository.findBy(pageable);
-  //    }
-  //    return trackRepository.findByIdAfter(cursor, pageable);
-  //  }
 
   public List<Track> findAll(int limit, Long cursor, List<TrackSort> sorts) {
     List<Sort.Order> sortOrders =
